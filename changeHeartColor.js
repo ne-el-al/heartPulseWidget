b09214d83fdeaa2505d3cbdc58bdeffe5c20036d
ne-el-al <e.a.negreeva@gmail.com>
2025-08-02 23:34:57 +0100

    keep the picked color on refresh

--- changeHeartColor.js
+++ changeHeartColor.js
@@ -1,20 +1,26 @@
 let colorPicker;
 const defaultColor = "#8B0000";
+const color = localStorage.getItem('color');
 
 window.addEventListener("load", startup, false);
 
 function startup() {
     colorPicker = document.querySelector("#color-picker");
-    colorPicker.value = defaultColor;
-    colorPicker.addEventListener("input", updateFirst, false);
+    if (color) {
+        colorPicker.value = color;
+        document.getElementsByTagName("h1")[0].style.color = color;
+    }
+    else {
+        colorPicker.value = defaultColor;
+    }
+    colorPicker.addEventListener("input", updateColor, false);
     colorPicker.select();
 }
 
-function updateFirst(event) {
+function updateColor(event) {
     const heart = document.getElementsByTagName("h1")[0];
     if (heart) {
         heart.style.color = event.target.value;
+        localStorage.setItem('color', event.target.value);
     }
 }
-
-
